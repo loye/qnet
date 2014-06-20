1832fa5073bb9e2e8e127a3dfbaea1030c28f07f
Loye <loye.qiu@live.com>
2014-06-20 18:02:48 +0800

    temp

---
+++ qnode.js
@@ -0,0 +1,101 @@
+var net = require('net'),
+    http = require('http'),
+    util = require('util'),
+    stream = require('stream'),
+    events = require("events"),
+    WebSocket = require('ws');
+
+
+var Node = (function () {
+    function _(options) {
+        events.EventEmitter.call(this);
+
+        this.EndPoints = [];
+
+        this.listen(options.port, options.host);
+    };
+    util.inherits(_, events.EventEmitter);
+
+
+
+    (function (proto) {
+        proto.listen = function (port, host) {
+            if (port && port > 0) {
+                this.EndPoints.push(new EndPoint(port, host, function () {
+                    console.log('start listening on', this.address);
+                }).on('connection', function (c) {
+                    console.log('new connection');
+                }));
+            }
+        };
+
+    })(_.prototype);
+
+    return _;
+})();
+
+var EndPoint = (function () {
+    function _(port, host, onlistening) {
+        events.EventEmitter.call(this);
+
+        var self = this;
+        if (typeof onlistening === 'function') {
+            this.on('listening', onlistening);
+        }
+        var webServer = http.createServer(function (req, res) {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('I\'m working');
+
+        }).listen(port, host, function () {
+            self.address = this.address();
+            self.server = new WebSocket.Server({ server: this });
+            self.server.on('connection', function (ws) {
+                self.emit('connection', new Connection(self, ws));
+            });
+            self.emit('listening');
+        });
+    };
+    util.inherits(_, events.EventEmitter);
+
+    return _;
+})();
+
+
+var Connection = (function () {
+    function _(node, ws) {
+        events.EventEmitter.call(this);
+
+        this.node = node;
+
+        ws.on('message', handleMessage);
+
+    };
+    util.inherits(_, events.EventEmitter);
+
+    function handleMessage(data, flags) {
+        console.log('receive message: ', data, flags);
+        flags.binary ? handleBinary() : handleText();
+
+    };
+    function handleText() {
+
+
+    };
+
+    function handleBinary() {
+
+
+    };
+
+    return _;
+})();
+
+
+exports = Node;
+
+
+
+if (require.main === module) {
+    var n = new Node({ port: 1337 });
+
+}
