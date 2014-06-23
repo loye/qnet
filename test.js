58fc834618a2b10f0e0f7f98b2f0e74e54320e13
Loye <loye.qiu@live.com>
2014-06-23 17:51:15 +0800

    temp

--- test.js
+++ test.js
@@ -1,10 +1,26 @@
+var net = require('net');
 var QNode = require('./lib/qnode.js');
-var WSConnection = require('./lib/connection.js');
+var WSConnection = require('./lib/ws-connection.js');
 
 
 WSConnection.connect('ws://localhost:1337').on('connection', function (s) {
     s.command({ type: 'test', value: 1 });
     s.on('command', function (c) {
-        console.log(typeof c.date);
+        console.log(c);
     });
+    process.stdin.pipe(s).pipe(process.stdout);
+});
+
+
+//var url1 = 'wss://qproxy.azurewebsites.net';
+//var port1 = 2001;
+//net.createServer(function (s) {
+//    WSConnection.connect(url1).on('connection', function (t) {
+//        s.pipe(t).pipe(s);
+//    });
+//}).listen(port1);
+//console.log('start listening on port ' + port1 + ', remote url: ' + url1);
+
+process.on('uncaughtException', function (err) {
+    console.log('[Error catched by process]' + err);
 });
