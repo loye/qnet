166ba35589ea3e772a183baec1b465fb655b1b34
Loye <loye.qiu@live.com>
2014-06-26 18:16:34 +0800

    temp

--- test.js
+++ test.js
@@ -3,28 +3,26 @@ var QNode = require('./lib/qnode.js');
 var WSConnection = require('./lib/ws-connection.js');
 
 
-//WSConnection.connect('ws://localhost:1337').on('connection', function (s) {
-//    s.command({ type: 'test', value: 1 });
-//    s.on('command', function (c) {
-//        console.log(c);
+WSConnection.connect('ws://localhost:1337/d').on('connection', function (s) {
+    s.message({ type: 'test', value: 1 });
+    s.on('message', function (m) {
+        console.log(m);
+    });
+    process.stdin.pipe(s).pipe(process.stdout);
+});
+
+
+//var url1 = 'wss://qproxy.azurewebsites.net';
+//var port1 = 2001;
+//net.createServer(function (s) {
+//    WSConnection.connect(url1).on('connection', function (t) {
+//        s.pipe(t).pipe(s);
+//        t.on('close', function () {
+//            console.log(t.trace);
+//        });
 //    });
-//    process.stdin.pipe(s).pipe(process.stdout);
-//});
-
-
-var url1 = 'wss://qproxy.azurewebsites.net';
-var port1 = 2001;
-net.createServer(function (s) {
-    var t = WSConnection.connect(url1);
-    s.pipe(t).pipe(s);
-    //WSConnection.connect(url1).on('connection', function (t) {
-    //    s.pipe(t).pipe(s);
-    //    t.on('close', function () {
-    //        console.log(t.trace);
-    //    });
-    //});
-}).listen(port1);
-console.log('start listening on port ' + port1 + ', remote url: ' + url1);
+//}).listen(port1);
+//console.log('start listening on port ' + port1 + ', remote url: ' + url1);
 
 process.on('uncaughtException', function (err) {
     console.log('[Error catched by process]' + err);
