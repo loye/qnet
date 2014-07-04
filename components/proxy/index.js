b098a106377494bcc9e78b117f3f11f1918fe087
Loye <loye.qiu@live.com>
2014-07-04 18:00:30 +0800

    temp

--- components/proxy/index.js
+++ components/proxy/index.js
@@ -1,11 +1,14 @@
 var proxy = require('./proxy_tunnel');
 
 module.exports.start = function (qnode, config) {
-    qnode.listen(config.path, function (conn) {
+    this._server = qnode.listen(config.path, function (conn) {
         conn.pipe(new proxy.ProxyTunnel(config.proxy)).pipe(conn);
     });
 };
 
 module.exports.stop = function (qnode, config) {
-
+    if (this._server) {
+        this._server.close();
+        delete this._server;
+    }
 };
