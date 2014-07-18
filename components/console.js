291c3bc5d1038045b922a3286eded4d4cbc426df
Loye <loye.qiu@live.com>
2014-07-18 13:21:35 +0800

    beta1

--- components/console.js
+++ components/console.js
@@ -1,34 +1,45 @@
 
 var Console = (function () {
-    function _(connections) {
+    function _(node, config) {
+        this.node = node;
+        this.config = config;
         this.ondata = this.input.bind(this);
         this.str = '';
     };
 
     function parse(str) {
-        var command, data, dataStr;
-        for (var i = 0; i < str.length; i++) {
-            if (str[i] !== '{' && str[i] !== '[') {
-                i++;
-            } else {
-                command = str.substring(0, i).trim();
-                dataStr = str.substring(i);
+        var command, data, dataStr, i = 0;
+        for (; i < str.length; i++) {
+            if (str[i] === '{' || str[i] === '[') {
                 break;
             }
         }
-        console.log(command);
-        console.log(dataStr);
-        if (!!command && !!dataStr) {
-            data = JSON.parse(dataStr, dateReviver);
+        command = str.substring(0, i).trim();
+        dataStr = str.substring(i);
+        if (command && dataStr) {
+            try {
+                data = JSON.parse(dataStr, dateReviver);
+            } catch (e) {
+                return null;
+            }
         }
-
-        return data ? { command: command, data: data } : null;
+        var result = command ? { command: command } : null;
+        result && data && (result.data = data);
+        return result;
     };
 
     function dateReviver(key, value) {
         return typeof value === 'string' && (e = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(?:\.(\d*))Z$/.exec(value))
             ? new Date(Date.UTC(+e[1], +e[2] - 1, +e[3], +e[4], +e[5], +e[6], +e[7]))
             : value;
+    };
+
+    function sendCommand(command, data, callback) {
+        var commandar = this.node.net.commandar;
+        if (commandar) {
+            callback.call(null, 'command accepted...');
+            commandar.execute(this, command, data);
+        }
     };
 
     (function (proto) {
@@ -43,16 +54,17 @@ var Console = (function () {
         };
 
         proto.input = function (data) {
-            var str = typeof data === 'string' ? data : data.toString('utf8');
+            var str = typeof data === 'string' ? data : data.toString();
             this.str += str;
             if (str === '\r\n') {
-                var str = this.str;
+                var str = this.str.trim();
                 this.str = '';
-                var command = parse(str);
-                console.log(command);
-                if (command) {
-                    console.log('command accepted');
-
+                var pkg = parse(str);
+                if (pkg) {
+                    console.log(pkg);
+                    sendCommand.call(this, pkg.command, pkg.data, function (result) {
+                        console.log(result);
+                    });
                 } else {
                     console.log('command not well formatted');
                 }
@@ -60,7 +72,10 @@ var Console = (function () {
         };
 
         proto.command = function (command, data, callback) {
-
+            console.log('command execute result:');
+            console.log(command);
+            console.log(data);
+            typeof callback === 'function' && callback.call(null);
         };
 
     })(_.prototype);
@@ -70,7 +85,7 @@ var Console = (function () {
 
 module.exports.component = {
     start: function (node, config) {
-        this._console = new Console().start();
+        this._console = new Console(node, config).start();
     },
     stop: function (node, config) {
         this._console.stop();
